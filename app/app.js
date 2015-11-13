b17927850d163ac7c60387272e5497d112f3c35f
Kevin Phillips <kevin.g.phillips@gmail.com>
2015-11-13 12:06:28 -0800

    app working for me now that I have CocoaPods installed.

--- app/app.js
+++ app/app.js
@@ -1,9 +1,9 @@
 var application = require("application");
 // google maps api key for ios
+application.mainModule = "main-page";
+application.cssFile = "./app.css";
 if(application.ios) {
   GMSServices.provideAPIKey("AIzaSyAjhlF70etTrM5fIGYfR_NlB_W5kXqE7Ng");
 }
-application.mainModule = "main-page";
-application.cssFile = "./app.css";
 application.start();
 
