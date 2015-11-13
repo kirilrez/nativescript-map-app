a70aab1a669623c836bea04f0ec30dba81be907b
Kevin Phillips <kevin.g.phillips@gmail.com>
2015-11-13 11:56:15 -0800

    Update app.js
    remove my personal api key, duh.

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 var application = require("application");
 // google maps api key for ios
 if(application.ios) {
-  GMSServices.provideAPIKey("AIzaSyAjhlF70etTrM5fIGYfR_NlB_W5kXqE7Ng");
+  GMSServices.provideAPIKey("YOUR_KEY_HERE");
 }
 application.mainModule = "main-page";
 application.cssFile = "./app.css";
