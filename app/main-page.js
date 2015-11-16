3965f327d6e171d2f10731944c845e90972aa24d
Kevin Phillips <kevin.g.phillips@gmail.com>
2015-11-16 13:22:28 -0800

    data binding to initialize map lat lng zoom from code behind functional

--- app/main-page.js
+++ app/main-page.js
@@ -3,14 +3,22 @@ var observableArrayModule = require("data/observable-array");
 
 var page;
 
+// var pageData = new observableModule.Observable({
+//      geoData:  new observableArrayModule.ObservableArray([   
+//         {
+//             latitude  : "46.185790", 
+//             longitude   : "-123.811471",
+//             zoom : "14"  
+//         }
+//      ])
+// });
+
 var pageData = new observableModule.Observable({
-     geoData:  new observableArrayModule.ObservableArray([   
-        {
-            latitude  : "46.185790", 
-            longitude   : "-123.811471",
-            zoom : "14"  
-        }
-     ])
+ 
+ latitude: "46.185790",
+ longitude: "-123.811471",
+ zoom: "12"
+
 });
 
 exports.pageLoaded = function(args) {
@@ -20,30 +28,27 @@ exports.pageLoaded = function(args) {
 
 };
 
-
 exports.OnMapReady= function(args) {
-  var mapView = args.object;
+      var mapView = args.object;
 
-  var gMap = mapView.gMap;
-  
-  
-  console.log("Setting a marker...");
- 
-    if(mapView.android) {
-        var markerOptions = new com.google.android.gms.maps.model.MarkerOptions();
-        markerOptions.title("Sydney");
-        markerOptions.snippet("Australia");
-        var latLng = new com.google.android.gms.maps.model.LatLng(-33.86, 151.20);
-        markerOptions.position(latLng);
-        gMap.addMarker(markerOptions);
-    } 
- 
-    if (mapView.ios) {
-        var position = CLLocationCoordinate2DMake(46.185790, -123.811471);
-        var marker = GMSMarker.markerWithPosition(CLLocationCoordinate2DMake(46.185790, -123.811471));
-        marker.title = "Astoria";
-        marker.snippet = "Oregon";
-        marker.map = gMap;
-    }
+      var gMap = mapView.gMap;
+      
+      console.log("Setting a marker...");
+     
+        // if(mapView.android) {
+        //     var markerOptions = new com.google.android.gms.maps.model.MarkerOptions();
+        //     markerOptions.title("Sydney");
+        //     markerOptions.snippet("Australia");
+        //     var latLng = new com.google.android.gms.maps.model.LatLng(-33.86, 151.20);
+        //     markerOptions.position(latLng);
+        //     gMap.addMarker(markerOptions);
+        // } 
+     
+        // if (mapView.ios) {
+        //     var position = CLLocationCoordinate2DMake(46.185790, -123.811471);
+        //     var marker = GMSMarker.markerWithPosition(CLLocationCoordinate2DMake(46.185790, -123.811471));
+        //     marker.title = "Astoria";
+        //     marker.snippet = "Oregon";
+        //     marker.map = gMap;
+        // }
 };
-
